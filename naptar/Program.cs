69ca3e8b20dbe66ddfa667d971e1c0dbb08a4107
raczr <raczrichardrobert@gmail.com>
2026-02-09 00:07:33 +0100

    Ujesemeny_letrehozasa

--- naptar/Program.cs
+++ naptar/Program.cs
@@ -128,6 +128,91 @@ namespace NaptariFeladat
             Console.WriteLine();
         }
 
+        static void UjEsemeny()
+        {
+            Console.WriteLine("\n--- Új esemény ---");
+
+            string ki = "";
+            while (ki != "apa" && ki != "anya")
+            {
+                Console.Write("Ki? (apa/anya): ");
+                ki = Console.ReadLine();
+                if (ki != "apa" && ki != "anya")
+                {
+                    Console.WriteLine("Csak apa vagy anya!");
+                }
+            }
+
+            int nap = 0;
+            while (nap < 1 || nap > 29)
+            {
+                Console.Write("Nap (1-29): ");
+                nap = int.Parse(Console.ReadLine());
+                if (nap < 1 || nap > 29)
+                {
+                    Console.WriteLine("1 és 29 között!");
+                }
+            }
+
+            int ora = 0;
+            while (ora < 8 || ora > 20)
+            {
+                Console.Write("Óra (8-20): ");
+                ora = int.Parse(Console.ReadLine());
+                if (ora < 8 || ora > 20)
+                {
+                    Console.WriteLine("8 és 20 között!");
+                }
+            }
+
+            int perc = -1;
+            while (perc < 0 || perc > 59)
+            {
+                Console.Write("Perc (0-59): ");
+                perc = int.Parse(Console.ReadLine());
+                if (perc < 0 || perc > 59)
+                {
+                    Console.WriteLine("0 és 59 között!");
+                }
+            }
+
+            int meddig = 0;
+            while (meddig < 30 || meddig > 120)
+            {
+                Console.Write("Időtartam percben (30-120): ");
+                meddig = int.Parse(Console.ReadLine());
+                if (meddig < 30 || meddig > 120)
+                {
+                    Console.WriteLine("30 és 120 között!");
+                }
+            }
+
+            DateTime mikor = new DateTime(2028, 2, nap, ora, perc, 0);
+
+            Esemeny e = new Esemeny();
+            e.Ki = ki;
+            e.Mikor = mikor;
+            e.Meddig = meddig;
+
+            lista.Add(e);
+
+            
+            for (int i = 0; i < lista.Count; i++)
+            {
+                for (int j = i + 1; j < lista.Count; j++)
+                {
+                    if (lista[i].Mikor > lista[j].Mikor)
+                    {
+                        Esemeny temp = lista[i];
+                        lista[i] = lista[j];
+                        lista[j] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("\nKész!\n");
+        }
+
         static void Menu()
         {
             Console.WriteLine("\n1 - Naptár");
