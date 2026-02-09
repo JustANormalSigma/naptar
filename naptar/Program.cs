b0f7c18bb43d078b2d1234c33480e556d1a74372
raczr <raczrichardrobert@gmail.com>
2026-02-09 20:30:19 +0100

    naptarV2_suliba csinalt

--- naptar/Program.cs
+++ naptar/Program.cs
@@ -5,164 +5,120 @@ using System.Linq;
 
 namespace NaptariFeladat
 {
-
     struct Esemeny
     {
-        public string Ki;
-        public DateTime Mikor;
-        public int Meddig;
+        public string tulajdonos;
+        public DateTime idopont;
+        public int idotartam;
     }
 
     class Program
     {
-        static List<Esemeny> lista = new List<Esemeny>();
+        static List<Esemeny> esemenyek = new List<Esemeny>();
+        static Random random = new Random();
 
         static void Main(string[] args)
         {
-            Console.OutputEncoding = System.Text.Encoding.UTF8;
-            Console.WriteLine("=== Családi naptár ===\n");
+            KezdetiFeltoltes();
 
-            Betoltes();
+            Console.WriteLine("Csaladi naptar program");
 
-            if (lista.Count == 0)
-            {
-                Console.WriteLine("Generálás...");
-                VeletlenEsemenyek();
-            }
             bool fut = true;
             while (fut)
             {
-                Menu();
+
+                Console.WriteLine("1 - Naptár megjelenítése");
+                Console.WriteLine("2 - Új esemény rögzítése");
+                Console.WriteLine("3 - Legközelebbi esemény megjelenítése");
+                Console.WriteLine("4 - Kilépés");
+
+                Console.Write("\nVálasszon menüpontot (1-4): ");
                 string valasztas = Console.ReadLine();
 
                 if (valasztas == "1")
                 {
-                    Naptar();
+                    NaptarMegjelenites();
                 }
-
                 else if (valasztas == "2")
                 {
                     UjEsemeny();
                 }
-
                 else if (valasztas == "3")
                 {
-                    Legkozelebbi();
+                    LegkozelabbiEsemeny();
                 }
-
                 else if (valasztas == "4")
                 {
-                    Console.WriteLine("\nViszlát!");
-                    Mentes();
+                    MentesFajlba();
+                    Console.WriteLine("Viszlát!");
                     fut = false;
                 }
                 else
                 {
-                    Console.WriteLine("\nRossz választás!");
+                    Console.WriteLine("Hibás választás! Válasszon 1 és 4 között!");
                 }
-
             }
         }
 
-
-        static void Betoltes()
+        static void KezdetiFeltoltes()
         {
-            try
+            string[] tulajdonosok = { "apa", "anya" };
+
+            for (int i = 0; i < 40; i++)
             {
-                StreamReader f = new StreamReader("esemenyek.txt");
+                Esemeny esemeny;
+                esemeny.tulajdonos = tulajdonosok[random.Next(2)];
 
-                string sor;
-                while ((sor = f.ReadLine()) != null)
-                {
-                    string[] darabok = sor.Split(';');
-                    string ki = darabok[0];
-                    string idopont = darabok[1];
-                    int meddig = int.Parse(darabok[2]);
+                int nap = random.Next(1, 30);
+                int ora = random.Next(8, 20);
+                int perc = random.Next(0, 60);
 
-                    string[] datum_ido = idopont.Split(' ');
-                    string[] datum = datum_ido[0].Split('-');
-                    string[] ido = datum_ido[1].Split(':');
+                esemeny.idopont = new DateTime(2028, 2, nap, ora, perc, 0);
+                esemeny.idotartam = random.Next(30, 121);
 
-                    int ev = int.Parse(datum[0]);
-                    int honap = int.Parse(datum[1]);
-                    int nap = int.Parse(datum[2]);
-                    int ora = int.Parse(ido[0]);
-                    int perc = int.Parse(ido[1]);
-
-                    DateTime mikor = new DateTime(ev, honap, nap, ora, perc, 0);
-
-                    Esemeny e = new Esemeny();
-                    e.Ki = ki;
-                    e.Mikor = mikor;
-                    e.Meddig = meddig;
-
-                    lista.Add(e);
-                }
-
-                f.Close();
-                Console.WriteLine("Betöltve!");
-            }
-            catch
-            {
-                Console.WriteLine("Nincs mentett fájl");
-
-            }
-
-        }
-        static void VeletlenEsemenyek()
-        {
-            Random rnd = new Random();
-
-            for (int i = 0; i < 20; i++)
-            {
-                string ki = i < 10 ? "apa" : "anya";
-
-                int nap = rnd.Next(1, 30);
-                int ora = rnd.Next(8, 20);
-                int perc = new int[] { 0, 15, 30, 45 }[rnd.Next(4)];
-
-                DateTime mikor = new DateTime(2028, 2, nap, ora, perc, 0);
-                int meddig = rnd.Next(30, 121);
-
-                Esemeny e = new Esemeny();
-                e.Ki = ki;
-                e.Mikor = mikor;
-                e.Meddig = meddig;
-
-                lista.Add(e);
+                esemenyek.Add(esemeny);
             }
         }
 
-        static void Naptar()
+        static void NaptarMegjelenites()
         {
-            if (lista.Count == 0)
+            if (esemenyek.Count == 0)
             {
-                Console.WriteLine("\nNincs esemény a naptárban");
+                Console.WriteLine("Nincs még esemény a naptárban.");
                 return;
             }
 
-            Console.WriteLine("\n--- Naptár 2028 február ---\n");
-            foreach (Esemeny e in lista)
+            List<Esemeny> rendezett = esemenyek.OrderBy(e => e.idopont).ToList();
+
+            Console.WriteLine("\n Csaladi Naptar 2028");
+            Console.WriteLine(new string('-', 60));
+
+            foreach (Esemeny esemeny in rendezett)
             {
-                Console.WriteLine($"{e.Ki} - {e.Mikor.Day}. {e.Mikor.Hour}:{e.Mikor.Minute:D2} - {e.Meddig} perc");
+                Console.WriteLine("Tulajdonos: " + esemeny.tulajdonos);
+                Console.WriteLine("Időpont: " + esemeny.idopont.ToString("yyyy.MM.dd. HH:mm"));
+                Console.WriteLine("Időtartam: " + esemeny.idotartam + " perc");
+                Console.WriteLine(new string('-', 60));
             }
-            Console.WriteLine();
         }
 
         static void UjEsemeny()
         {
-            Console.WriteLine("\n--- Új esemény ---");
+            Console.WriteLine("\nÚJ ESEMÉNY RÖGZÍTÉSE");
 
-            string ki = "";
-            while (ki != "apa" && ki != "anya")
+            Esemeny esemeny;
+
+            string tulajdonos = "";
+            while (tulajdonos != "apa" && tulajdonos != "anya")
             {
-                Console.Write("Ki? (apa/anya): ");
-                ki = Console.ReadLine();
-                if (ki != "apa" && ki != "anya")
+                Console.Write("Tulajdonos (apa/anya): ");
+                tulajdonos = Console.ReadLine().ToLower();
+                if (tulajdonos != "apa" && tulajdonos != "anya")
                 {
-                    Console.WriteLine("Csak apa vagy anya!");
+                    Console.WriteLine("Hibás érték! Csak 'apa' vagy 'anya' lehet!");
                 }
             }
+            esemeny.tulajdonos = tulajdonos;
 
             int nap = 0;
             while (nap < 1 || nap > 29)
@@ -171,7 +127,7 @@ namespace NaptariFeladat
                 nap = int.Parse(Console.ReadLine());
                 if (nap < 1 || nap > 29)
                 {
-                    Console.WriteLine("1 és 29 között!");
+                    Console.WriteLine("Hibás nap! 1 és 29 között adjon meg!");
                 }
             }
 
@@ -182,7 +138,7 @@ namespace NaptariFeladat
                 ora = int.Parse(Console.ReadLine());
                 if (ora < 8 || ora > 20)
                 {
-                    Console.WriteLine("8 és 20 között!");
+                    Console.WriteLine("Hibás óra! 8 és 20 között adjon meg!");
                 }
             }
 
@@ -193,112 +149,90 @@ namespace NaptariFeladat
                 perc = int.Parse(Console.ReadLine());
                 if (perc < 0 || perc > 59)
                 {
-                    Console.WriteLine("0 és 59 között!");
+                    Console.WriteLine("Hibás perc! 0 és 59 között adjon meg!");
                 }
             }
 
-            int meddig = 0;
-            while (meddig < 30 || meddig > 120)
+            int idotartam = 0;
+            while (idotartam < 30 || idotartam > 120)
             {
                 Console.Write("Időtartam percben (30-120): ");
-                meddig = int.Parse(Console.ReadLine());
-                if (meddig < 30 || meddig > 120)
+                idotartam = int.Parse(Console.ReadLine());
+                if (idotartam < 30 || idotartam > 120)
                 {
-                    Console.WriteLine("30 és 120 között!");
+                    Console.WriteLine("Hibás időtartam! 30 és 120 perc között adjon meg!");
                 }
             }
 
-            DateTime mikor = new DateTime(2028, 2, nap, ora, perc, 0);
+            esemeny.idopont = new DateTime(2028, 2, nap, ora, perc, 0);
+            esemeny.idotartam = idotartam;
 
-            Esemeny e = new Esemeny();
-            e.Ki = ki;
-            e.Mikor = mikor;
-            e.Meddig = meddig;
+            esemenyek.Add(esemeny);
+            Console.WriteLine("\nAz esemény sikeresen rögzítve!");
+        }
 
-            lista.Add(e);
-
-            
-            for (int i = 0; i < lista.Count; i++)
+        static void LegkozelabbiEsemeny()
+        {
+            if (esemenyek.Count == 0)
             {
-                for (int j = i + 1; j < lista.Count; j++)
+                Console.WriteLine("Nincs esemény a naptárban.");
+                return;
+            }
+
+            int refNap = random.Next(1, 30);
+            int refOra = random.Next(0, 24);
+            int refPerc = random.Next(0, 60);
+            DateTime referencia = new DateTime(2028, 2, refNap, refOra, refPerc, 0);
+
+            Console.WriteLine("\n LEGKÖZELEBBI ESEMÉNY ");
+            Console.WriteLine("Viszonyítási időpont: " + referencia.ToString("yyyy.MM.dd. HH:mm"));
+            Console.WriteLine(new string('-', 60));
+
+            bool talalat = false;
+            Esemeny legkozelebbi = new Esemeny();
+            TimeSpan legkisebbKulonbseg = TimeSpan.MaxValue;
+
+            foreach (Esemeny esemeny in esemenyek)
+            {
+                if (esemeny.idopont >= referencia)
                 {
-                    if (lista[i].Mikor > lista[j].Mikor)
+                    TimeSpan kulonbseg = esemeny.idopont - referencia;
+
+                    if (!talalat || kulonbseg < legkisebbKulonbseg)
                     {
-                        Esemeny temp = lista[i];
-                        lista[i] = lista[j];
-                        lista[j] = temp;
+                        legkozelebbi = esemeny;
+                        legkisebbKulonbseg = kulonbseg;
+                        talalat = true;
                     }
                 }
             }
 
-            Console.WriteLine("\nKész!\n");
-        }
-
-
-        static void Mentes()
-        {
-            try
+            if (!talalat)
             {
-                StreamWriter f = new StreamWriter("esemenyek.txt");
-
-                foreach (Esemeny e in lista)
-                {
-                    f.WriteLine($"{e.Ki};{e.Mikor.Year}-{e.Mikor.Month}-{e.Mikor.Day} {e.Mikor.Hour}:{e.Mikor.Minute};{e.Meddig}");
-                }
-
-                f.Close();
-                Console.WriteLine("Mentve!");
-            }
-            catch
-            {
-                Console.WriteLine("Hiba a mentés során!");
-            }
-        }
-
-        static void Legkozelebbi()
-        {
-            if (lista.Count == 0)
-            {
-                Console.WriteLine("\nNincs esemény");
-                return;
-            }
-
-            Random rnd = new Random();
-            int nap = rnd.Next(1, 30);
-            int ora = rnd.Next(0, 24);
-            int perc = rnd.Next(0, 60);
-            DateTime most = new DateTime(2028, 2, nap, ora, perc, 0);
-
-            Console.WriteLine($"\nMost: {most.Day}. {most.Hour}:{most.Minute:D2}");
-
-            Esemeny? talalat = null;
-            foreach (Esemeny e in lista)
-            {
-                if (e.Mikor >= most)
-                {
-                    talalat = e;
-                    break;
-                }
-            }
-
-            if (talalat != null)
-            {
-                Esemeny t = talalat.Value;
-                Console.WriteLine($"Következő: {t.Ki} - {t.Mikor.Day}. {t.Mikor.Hour}:{t.Mikor.Minute:D2} - {t.Meddig} perc\n");
+                Console.WriteLine("Nincs a viszonyítási időpont után esemény.");
             }
             else
             {
-                Console.WriteLine("Nincs több esemény\n");
+                Console.WriteLine("Tulajdonos: " + legkozelebbi.tulajdonos);
+                Console.WriteLine("Időpont: " + legkozelebbi.idopont.ToString("yyyy.MM.dd. HH:mm"));
+                Console.WriteLine("Időtartam: " + legkozelebbi.idotartam + " perc");
             }
         }
 
-        static void Menu()
+        static void MentesFajlba()
         {
-            Console.WriteLine("\n1 - Naptár");
-            Console.WriteLine("2 - Új esemény");
-            Console.WriteLine("3 - Legközelebbi esemény");
-            Console.WriteLine("4 - Kilépés");
-            Console.Write("\nVálassz: ");
+            StreamWriter fajl = new StreamWriter("naptar.txt");
+
+            foreach (Esemeny esemeny in esemenyek)
+            {
+                string sor = esemeny.tulajdonos + ";" +
+                             esemeny.idopont.ToString("yyyy.MM.dd. HH:mm") + ";" +
+                             esemeny.idotartam;
+                fajl.WriteLine(sor);
+            }
+
+            fajl.Close();
+            Console.WriteLine("\nAz események sikeresen mentve a naptar.txt fájlba!");
         }
     }
 }
